28462bfd94d31e2f580fb84f0eea62a5ac5fbba0
Alex Tsague <alextsague98@gmail.com>
2022-05-26 15:55:27 +0200

    Initialisation

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//Routes
+app.listen(5000)
