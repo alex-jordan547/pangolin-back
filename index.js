849ebdd592b10d5cd520cfff300b49e837f9cfe4
Alex Tsague <alextsague98@gmail.com>
2022-05-26 16:44:25 +0200

    Configuration de la BDD et methodes primaires dans le MongoRepository.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const database = require('./MongoRepository')
+var db;
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -8,6 +10,16 @@ app.use((req, res, next) => {
 });
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+//Connexion à la bdd
+database.connect().then(datab=>{
+    console.log('connected')
+    db = datab;
+})
 
 //Routes
+
+
+
+
+
 app.listen(5000)
